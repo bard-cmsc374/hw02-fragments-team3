ac24de94d4b34f9601ca69070d4a03c29ef5ef0c
Marley Alford <ma8642@bard.edu>
2016-09-25 17:12:41 -0400

    Listview fragment is successfully connected to main activity view.  List values are not wired up to the add button yet, though.

--- app/src/main/java/edu/bard/todolist_lab1/ToDoList.java
+++ app/src/main/java/edu/bard/todolist_lab1/ToDoList.java
@@ -4,7 +4,6 @@ package edu.bard.todolist_lab1;
  */
 import  java.util.ArrayList;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
