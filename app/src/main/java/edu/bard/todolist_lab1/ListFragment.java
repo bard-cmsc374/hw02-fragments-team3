17b0f11da36fb6f0e07225665a786bccf44c223c
Marley Alford <ma8642@bard.edu>
2016-09-25 17:55:31 -0400

    Listview fragment is successfully connected to main activity view.  List values are not wired up to the add button yet, though.

--- app/src/main/java/edu/bard/todolist_lab1/ListFragment.java
+++ app/src/main/java/edu/bard/todolist_lab1/ListFragment.java
@@ -5,15 +5,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by MarleyAlford on 9/25/16.
  */
 public class ListFragment extends Fragment{
+    private ArrayList<String> mToDoItems; // list of items
+    private ArrayAdapter<String> aa; // adapter from list to viewlist
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mToDoItems = new ArrayList<String>();
+        aa = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, mToDoItems);
+    }
+
+    public void addNewItem(String newItem){  //this function will be called by addItemFragment whenever a new list item is added
+        mToDoItems.add(0, newItem.toString());
+        aa.notifyDataSetChanged();
     }
 
     @Override
--- app/src/main/java/edu/bard/todolist_lab1/ToDoList.java
+++ app/src/main/java/edu/bard/todolist_lab1/ToDoList.java
@@ -9,19 +9,14 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 
 public class ToDoList extends FragmentActivity {
     public static String TAG = "todolab";
-    private EditText mEditText; // enter todo item
-    private Button mItemButton; // add the item to the list
-    private ArrayList<String> mToDoItems; // list of items
-    private ArrayAdapter<String> aa; // adapter from list to viewlist
-    private ListView mListView;
+    //private ArrayList<String> mToDoItems; // list of items
+    //private ArrayAdapter<String> aa; // adapter from list to viewlist
+    //private ListView mListView;
 
     @Override
     public void onCreate(Bundle stuff) {
@@ -32,36 +27,36 @@ public class ToDoList extends FragmentActivity {
 
         //Fragment manager will wire fragments to main view
         FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container_list);
 
         if (fragment == null) {  //commits a fragment transaction to main screen
             fragment = new ListFragment();
             fm.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
+                    .add(R.id.fragment_container_list, fragment)
                     .commit();
         }
 
         // Get references to UI widgets
-        mEditText = (EditText) findViewById(R.id.myEditText);
-        mItemButton = (Button) findViewById(R.id.addButton);
+        //mEditText = (EditText) findViewById(R.id.myEditText);
+        //mItemButton = (Button) findViewById(R.id.addButton);
         //mListView = (ListView) findViewById(R.id.myListView);
 
         // Create the ArrayList and the ArrayAdapter
-        mToDoItems = new ArrayList<String>();
-        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mToDoItems);
+//        mToDoItems = new ArrayList<String>();
+//        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mToDoItems);
 
         // Bind the listview to the array adapter
         //mListView.setAdapter(aa);
 
         // Add key listener to add the new todo item
         // when the middle D-pad button is pressed.
-        mItemButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                mToDoItems.add(0, mEditText.getText().toString());
-                aa.notifyDataSetChanged();
-                mEditText.setText("");
-            }
-        });
+//        mItemButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View view) {
+//                mToDoItems.add(0, mEditText.getText().toString());
+//                aa.notifyDataSetChanged();
+//                mEditText.setText("");
+//            }
+//        });
 
         Log.i(TAG, "Entered onCreate");
     }
