6ff1215c7edfb91693f5516d04f5c59a17f7a709
Marley Alford <ma8642@bard.edu>
2016-09-25 16:58:11 -0400

    Listview fragment is successfully connected to main activity view.  List values are not wired up to the add button yet, though.

---
+++ app/src/main/java/edu/bard/todolist_lab1/ListFragment.java
@@ -0,0 +1,26 @@
+package edu.bard.todolist_lab1;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by MarleyAlford on 9/25/16.
+ */
+public class ListFragment extends Fragment{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_list, container, false);
+        return v;
+    }
+
+}
--- app/src/main/java/edu/bard/todolist_lab1/ToDoList.java
+++ app/src/main/java/edu/bard/todolist_lab1/ToDoList.java
@@ -2,10 +2,13 @@ package edu.bard.todolist_lab1;
 /*
   Most basic Todo List in one activity, no fragments.
  */
-import java.util.ArrayList;
+import  java.util.ArrayList;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -13,7 +16,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-public class ToDoList extends Activity {
+public class ToDoList extends FragmentActivity {
     public static String TAG = "todolab";
     private EditText mEditText; // enter todo item
     private Button mItemButton; // add the item to the list
@@ -28,17 +31,28 @@ public class ToDoList extends Activity {
         // Inflate your view
         setContentView(R.layout.main); // Extracts resources, autogenerates R.java from XML file
 
+        //Fragment manager will wire fragments to main view
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {  //commits a fragment transaction to main screen
+            fragment = new ListFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+
         // Get references to UI widgets
         mEditText = (EditText) findViewById(R.id.myEditText);
         mItemButton = (Button) findViewById(R.id.addButton);
-        mListView = (ListView) findViewById(R.id.myListView);
+        //mListView = (ListView) findViewById(R.id.myListView);
 
         // Create the ArrayList and the ArrayAdapter
         mToDoItems = new ArrayList<String>();
         aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mToDoItems);
 
         // Bind the listview to the array adapter
-        mListView.setAdapter(aa);
+        //mListView.setAdapter(aa);
 
         // Add key listener to add the new todo item
         // when the middle D-pad button is pressed.
