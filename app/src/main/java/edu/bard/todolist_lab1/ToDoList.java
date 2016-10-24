554fa65521f83c9ca8a55b8e94f15c8e9f0b85bb
Sven Anderson <sveneanderson@gmail.com>
2016-10-23 23:08:51 -0400

    HW #2 Feedback

--- app/src/main/java/edu/bard/todolist_lab1/ToDoList.java
+++ app/src/main/java/edu/bard/todolist_lab1/ToDoList.java
@@ -1,6 +1,50 @@
 package edu.bard.todolist_lab1;
 /*
   Most basic Todo List in one activity, no fragments.
+
+
+CMSC 374
+HW #2 Fragments: Feedback
+
+Names: Marley and Meghan
+
+      Rubric
+----------------------------------------------------------
+40	1. App functions correctly from end user perspective.
+20	2. Fragments are correctly created.
+10	3. Fragments share information on the adapter arraylist.
+10	4. Code is clearly commented.
+20	5. Code is readable and well constructed.
+
+Score: 100
+
+
+----------------
+
+Comments
+
+My comments in your code are marked with XX in a comment.
+
+
+1. App functions correctly from end user perspective. YES, resources used, too.
+
+2. Fragments are correctly created. YES and managed
+
+3. Fragments share information on the adapter arraylist.
+
+You allow the activity to call the list's update, which is fine for
+this exercise.
+
+4. Code is clearly commented.
+You remark on all important changes.
+
+5. Code is readable and well constructed.
+Yes...good throughout.
+
+
+
+
+
  */
 import java.util.ArrayList;
 
@@ -61,7 +105,7 @@ public class ToDoList extends FragmentActivity implements AddItemFragment.ItemCa
         public void setItem (String item) {
             //implementation of interface. Gets item and sets value of mItem
             //calls ListFragment's addNewItem method
-            mItem = item;
+            mItem = item; // XX good, this works properly.
             ListFragment listFrag = (ListFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_container_list);
             listFrag.addNewItem(mItem);
             //Log.i(TAG, "item is " + mItem);
